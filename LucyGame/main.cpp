b60ef2d811da6fa33105bfd8bc6dc75ccf308e05
SuKiKaGe <SuKiKaGe@gmail.com>
2016-11-10 16:27:40 +0100

    Initialization

---
+++ LucyGame/main.cpp
@@ -0,0 +1,16 @@
+// LucyGame
+
+//#include <SDL.h>
+
+int main(int argc, char ** argv)
+{
+	/*
+	SDL_Init(SDL_INIT_VIDEO);
+
+	// game code eventually goes here
+
+	SDL_Quit();
+	*/
+
+	return 0;
+}
